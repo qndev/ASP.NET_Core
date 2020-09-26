8ccd76cd08a26b5e784900d443dcaf7e0a284c32
Dinh Quang <quangnd.hust@gmail.com>
2020-09-26 09:50:08 -0400

    setup sln and  project

---
+++ ASP.NET_Core.MvcWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ASP.NET_Core.MvcWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
